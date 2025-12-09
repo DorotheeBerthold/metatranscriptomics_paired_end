79f0fd682d1a1216cef74314eb34a09d84a5c5ba
Dorothée L. Berthold <152390074+DorotheeBerthold@users.noreply.github.com>
2025-12-09 16:49:34 +0100

    Add KEGG enrichment analysis and plotting
    This script creates KEGG enrichment plots for upregulated and downregulated genes, summarizes community-level data, and saves the resulting plots.

--- 13_kegg_enrichment_plots.R
+++ 13_kegg_enrichment_plots.R
@@ -114,3 +114,78 @@ for (org in org_list) {
   
   message("Finished: ", org, " → saved ", out_png)
 }
+
+# Create community level summary (optional) ---------------------------------------------
+
+community_kegg_list <- lapply(org_list, function(org) {
+  read.csv(paste0("results/DE_genes_", org, "_annotated_KOs.csv"))
+})
+
+# Combine all into one big dataframe
+community_df <- dplyr::bind_rows(community_kegg_list)
+
+upregulated <- community_df %>% 
+  filter(diffexpressed == "+fiber")
+
+downregulated <- community_df %>% 
+  filter(diffexpressed == "-fiber")
+
+up_kegg <- split_kegg(upregulated$kegg_id)
+down_kegg <- split_kegg(downregulated$kegg_id)
+
+# -------------------------------
+# KEGG enrichment
+# -------------------------------
+enrich_up <- enrichKEGG(
+  gene = up_kegg,
+  organism = "ko",
+  keyType = "kegg",
+  pvalueCutoff = 0.05
+  # universe = background_kegg
+)
+
+enrich_down <- enrichKEGG(
+  gene = down_kegg,
+  organism = "ko",
+  keyType = "kegg",
+  pvalueCutoff = 0.05
+  # universe = background_kegg
+)
+
+# -------------------------------
+# Convert to df
+# -------------------------------
+up_df <- enrich_up@result %>% 
+  mutate(direction = "+ Fiber")
+
+down_df <- enrich_down@result %>% 
+  mutate(direction = "- Fiber")
+
+# -------------------------------
+# Combine
+# -------------------------------
+enrich_combined <- bind_rows(up_df, down_df)
+
+# Keep top 20 per group
+enrich_combined <- enrich_combined %>%
+  group_by(direction) %>%
+  slice_min(pvalue, n = 20) %>%
+  ungroup()
+
+ggplot(enrich_combined,
+       aes(x = Count,
+           y = reorder(Description, -pvalue),
+           size = Count,
+           color = p.adjust)) +
+  geom_point() +
+  scale_color_continuous(trans = "log10", name = "Adjusted p-value") +
+  facet_wrap(~ direction, scales = "free_y") +
+  theme_bw() +
+  labs(
+    title = paste("KEGG Pathway Enrichment Community"),
+    x = "Gene count",
+    y = "Pathway"
+  )
+# Save figure
+ggsave("plots/KEGG_pathway_enrichment_community.png", width = 10, height = 6)
+
